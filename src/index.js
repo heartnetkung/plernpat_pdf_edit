04f0299b17b5808d31d068ce86b1f20b2d1f2976
heartnetkung <heartnetkung@gmail.com>
2022-08-29 15:57:21 +0700

    change port

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.get("/download", (req, res) => {
 	);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static("../data")).listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
